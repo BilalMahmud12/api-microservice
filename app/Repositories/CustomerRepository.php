e3d90fdb64bc2f74bae96aafca0be5ab796a015b
Bilal <bm@fabricdata.com>
2024-11-23 23:58:25 +0300

    Repository Methods implementation

--- app/Repositories/CustomerRepository.php
+++ app/Repositories/CustomerRepository.php
@@ -10,26 +10,56 @@ class CustomerRepository implements CustomerRepositoryInterface
 {
     public function getAllCustomers(int $page, int $perPage)
     {
-        //
+        return Customer::paginate($perPage, ['*'], 'page', $page);
     }
 
     public function getCustomerById(int $id)
     {
-        //
+        return Customer::find($id);
     }
 
     public function createCustomer(CreateCustomerDTO $data)
     {
-        //
+        $customer = new Customer();
+        $customer->name = $data->name;
+        $customer->surname = $data->surname;
+        $customer->balance = $data->balance;
+
+        $customer->save();
+
+        return $customer;
     }
 
     public function updateCustomer(int $id, array $data)
     {
-        //
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return null;
+        }
+
+        if (array_key_exists('name', $data)) {
+            $customer->name = $data['name'];
+        }
+
+        if (array_key_exists('surname', $data)) {
+            $customer->surname = $data['surname'];
+        }
+
+        $customer->save();
+
+        return $customer;
     }
 
     public function deleteCustomer(int $id)
     {
-        //
+        $customer = Customer::find($id);
+
+        if ($customer) {
+            $customer->delete();
+            return true;
+        }
+
+        return false;
     }
 }
